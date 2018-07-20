0c0e8402dd4df6cdcc51287bdae87c6e79fdc261
Abhijit <abhijitfulsagar@github.com>
2018-07-20 11:33:18 -0700

    cleaning dataset

--- RestuarantReview.py
+++ RestuarantReview.py
@@ -11,3 +11,13 @@ import matplotlib.pyplot as plt
  
 # importing dataset
 dataset = pd.read_csv(r'''D:\MS\UDEMY COURSES\Machine Learning\Machine_Learning_AZ_Template_Folder\Machine Learning A-Z Template Folder\Part 7 - Natural Language Processing\Section 36 - Natural Language Processing\Restaurant_Reviews.tsv''',delimiter='\t',quoting=3)
+
+#c cleaning texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0])
+review = review.lower()
+review = review.split()
+review = [word for word in review if not word in set(stopwords.words('english'))]
