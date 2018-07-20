50ae612e527ee7287bb3de6714db90214cf5b3e3
Abhijit <abhijitfulsagar@github.com>
2018-07-20 10:48:10 -0700

    Importing dataset

---
+++ RestuarantReview.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 20 10:09:59 2018
+
+@author: ABHIJIT
+"""
+# importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# importing dataset
+dataset = pd.read_csv(r'''D:\MS\UDEMY COURSES\Machine Learning\Machine_Learning_AZ_Template_Folder\Machine Learning A-Z Template Folder\Part 7 - Natural Language Processing\Section 36 - Natural Language Processing\Restaurant_Reviews.tsv''',delimiter='\t',quoting=3)
